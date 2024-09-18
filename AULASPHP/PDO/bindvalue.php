69e8017296f40691a2dbf6651d045f91a5c7d1a9
Jussara Pacheco <jussarahrosah@gmail.com>
2024-09-18 15:04:41 -0300

    exercicio2

---
+++ AULASPHP/PDO/bindvalue.php
@@ -0,0 +1,49 @@
+<?php
+
+$host = 'localhost';
+$dbname = 'bancodedados';
+$username = 'root';
+$password = '';
+
+//cria conexão com banco de dados PDO - PHP Data Objects
+
+
+try{
+    $conn = new PDO("mysql:host = $host;dbname=$dbname",$username,$password);
+
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Conexão realizada com sucesso<br>";
+}
+catch (PDOException $e){
+    echo "Falha na conexão:".$e->getmessage();
+}
+
+//dados a serem inseridos
+
+$nome = 'cafe';
+$preco = '9.99';
+
+$sql = "INSERT INTO produtos(nome,preco) VALUES (:nome,:preco)";
+
+//prepara a conexão
+
+$stmt = $conn->prepare($sql);
+
+//associa os valores aos parametros de consulta
+$stmt->bindValue(':nome',$nome);
+$stmt->bindValue(':preco',$preco);
+
+// executa a consulta
+
+if($stmt->execute()){
+    echo "dados inseridos com sucesso";
+}
+else{
+    echo "Erro ao inserir dados";
+}
+
+//fecha a conexão
+
+$conn = null;
+
+?>
